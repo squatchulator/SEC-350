f7da9e857908179a8a9fb29d1452509f7fa23bcb
squatchulator <miles.cummings@mymail.champlain.edu>
2023-11-12 18:29:03 +0000

    Working script!

--- wg-install.sh
+++ wg-install.sh
@@ -1,42 +1,30 @@
 #!/bin/bash
-# https://www.wireguard.com/quickstart/
-# https://www.digitalocean.com/community/tutorials/how-to-set-up-wireguard-on-ubuntu-20-04 
-# ChatGPT
-# https://www.freecodecamp.org/news/build-your-own-wireguard-vpn-in-five-minutes/ 
-
-# TO INSTALL: wget -O wg-install.sh https://raw.githubusercontent.com/squatchulator/SEC-350/master/wg-install.sh
-
 
 sudo apt update
 sudo apt install wireguard -y
 
-# Enable port forwarding on this guy
 echo 'net.ipv4.ip_forward=1' | sudo tee -a /etc/sysctl.conf
 sudo sysctl -p
-# Create the configuration files
-sudo mkdir /etc/wireguard
+
 sudo touch /etc/wireguard/wg0.conf
 sudo touch /etc/wireguard/wg0-client.conf
-# Create the env variables for my git creds so the whole process is automated.
-# NOTE this passkey is set to expire in 7 days as of 11/11
+
 export USERNAME="squatchulator"
-export PASSKEY="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"
-# Clone my repo so we can add configs to it later (GPT was very helpful figuring this out)
+export PASSKEY="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"
+
 cd
-git clone https://${USERNAME}:${PASSKEY}@github.com/squatchulator/SEC-350.git
-# Allow for read/write/execute of all contents in wireguard directory
+git clone https://$USERNAME:$PASSKEY@github.com/squatchulator/SEC-350.git
+
 sudo chmod -R +wrx /etc/wireguard
-# Keygen for the server
-wg genkey | tee /etc/wireguard/server-priv | wg pubkey > /etc/wireguard/server-pub
-sleep 3
+
+sudo wg genkey | sudo tee /etc/wireguard/server-priv | sudo wg pubkey > /etc/wireguard/server-pub
 export SERVERPRIV=$(sudo cat /etc/wireguard/server-priv)
 export SERVERPUB=$(sudo cat /etc/wireguard/server-pub)
-# Keygen for the client
-wg genkey | tee /etc/wireguard/client-priv | wg pubkey > /etc/wireguard/client-pub
-sleep 3
+
+sudo wg genkey | sudo tee /etc/wireguard/client-priv | sudo wg pubkey > /etc/wireguard/client-pub
 export CLIENTPRIV=$(sudo cat /etc/wireguard/client-priv)
 export CLIENTPUB=$(sudo cat /etc/wireguard/client-pub)
-# Edit the config file 
+
 sudo tee /etc/wireguard/wg0.conf > /dev/null <<EOF
 [Interface]
 PrivateKey = $SERVERPRIV
@@ -53,11 +41,11 @@ PostDown = iptables -t nat -D POSTROUTING -o ens160 -j MASQUERADE
 PublicKey = $CLIENTPUB
 AllowedIPs = 10.8.0.2/24
 EOF
-# Allow the port through the local firewall, and restart it
+
 sudo ufw allow 7890/udp
 sudo ufw disable
 sudo ufw enable
-# Now, we need a config file for the client.
+
 sudo tee /etc/wireguard/wg0-client.conf > /dev/null <<EOF
 [Interface]
 PrivateKey = $CLIENTPRIV
@@ -69,11 +57,8 @@ AllowedIPs = 10.8.0.1/24
 Endpoint = 10.0.17.114:7890
 EOF
 
-# Start the server
-sudo systemctl enable wg-quick@wg0.service
-sudo systemctl daemon-reload
-sudo systemctl start wg-quick@wg0
-# Time for git stuff.
+sudo wg-quick up wg0
+
 cd
 sudo cp -R /etc/wireguard SEC-350
 cd SEC-350
@@ -83,18 +68,3 @@ git config --global user.email "miles.cummings@mymail.champlain.edu"
 git config --global user.name "squatchulator"
 git commit -m "Automated push of Wireguard configs"
 git push
-# Now your configs should be accessable at:
-# https://github.com/squatchulator/SEC-350 under the wireguard folder.
-
-# Go to the repo and download the zip of the code. 
-# Download wireguard from the official website
-# Once installed, open wireguard and at the bottom left, click "Add Tunnel"
-# Now, navigate to the folder we downloaded and select "wg0-client.conf"
-# All necessary info should be automatically imported. Click "Activate".
-
-
-
-
-
-
-
